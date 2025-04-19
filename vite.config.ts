86dd3764f06b38d4f453ca8a8e9e082790f62aa9
Marvin Hackfort <marvin.hackfort@gmail.com>
2025-04-19 14:43:51 +0200

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tunequest-card-generator/', // <--- HIER wichtig!
   build: {
     terserOptions: {
       compress: {
